99191398193045b77a1d1912dbd5c5a358bfe23d
just-cady <cady@notnatural.co>
2018-05-09 13:08:54 -0500

    Added timeout handling
    Show an error message if the db binary takes longer than 7 seconds to
    load.

--- js/parse.js
+++ js/parse.js
@@ -10,11 +10,13 @@ var questions = [];
 // 7 - Library
 
 function init() {
-    var xhr = new XMLHttpRequest();
+    let timeout = true;
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://raw.githubusercontent.com/systers/powerup-iOS/develop/Scenarios/mainDatabase.sqlite', true);
     xhr.responseType = 'arraybuffer';
 
     xhr.onload = function (e) {
+        timeout = false;
         let loader = document.getElementById("loader");
         if (xhr.status === 200) {
             let uInt8Array = new Uint8Array(this.response);
@@ -52,6 +54,13 @@ function init() {
         }
     };
     xhr.send();
+
+    setTimeout(() => {
+        if (timeout) {
+            console.log("Timed out")
+            loader.innerHTML = "<br><br><br><br><br> There was a problem loading the database.<br><br>Please try again!";
+        }
+    }, 7000);
 }
 
 function parse(currentScenario) {
